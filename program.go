cbb1b79c456ef1d828ea5913dbcde9bf3edfc4af
Marcos <marcos_rodrigues@icloud.com>
2016-08-20 14:22:02 -0300

    Version 1.0

--- program.go
+++ program.go
@@ -38,44 +38,71 @@ func updateDB() {
 
 func main() {
 
-	if len(os.Args) <= 1 {
-		fmt.Println("Uso: $ chsht python | less -r, para atualizar: $ chsht -update")
-		os.Exit(0)
-	}
-
-	if _, err := os.Stat(os.Getenv("HOME") + "/.cheat_sheets.db"); os.IsNotExist(err) {
-		updateDB()
-		fmt.Println("Banco de dados instalado com sucesso!")
-	}
-
-	if os.Args[1] == "-update" {
-		updateDB()
-		fmt.Println("Banco de dados atualizado com sucesso!")
-		os.Exit(0)
-	}
-
-	Program := os.Args[1]
-
-	db, _ := sql.Open("sqlite3", os.Getenv("HOME")+"/.cheat_sheets.db")
-	rows, _ := db.Query("SELECT * FROM " + Program)
+	db, err := sql.Open("sqlite3", os.Getenv("HOME")+"/.cheat_sheets.db")
+	check(err)
 	dbtables, err := db.Query("select name from sqlite_master where type = 'table'")
 	check(err)
 
 	tables := []string{}
+
 	for dbtables.Next() {
 		var name string
 		_ = dbtables.Scan(&name)
 		tables = append(tables, name)
 	}
 
-	if containsStr(tables, Program) != true {
-		fmt.Println("Sofware nao existe na base da dados.")
-		fmt.Println("Disponiveis:")
+	if len(os.Args) <= 1 {
+		fmt.Println("Uso: $ chsht python | less -r, para atualizar: $ chsht --update")
+		os.Exit(0)
+	}
+
+	if _, err := os.Stat(os.Getenv("HOME") + "/.cheat_sheets.db"); os.IsNotExist(err) {
+		updateDB()
+	}
+
+	if os.Args[1] == "--update" {
+		updateDB()
+		fmt.Println("Database updated!")
+		os.Exit(0)
+	}
+
+	if os.Args[1] == "--version" {
+		fmt.Println("chsht 1.0")
+		os.Exit(0)
+	}
+
+	if os.Args[1] == "--help" {
+
+		opt_update := []string{"--update", "Update database, internet connection is required."}
+		fmt.Println(strings.Join(opt_update, "\n\t"))
+		opt_ls := []string{"--list", "List sofwares with cheat sheets in database."}
+		fmt.Println(strings.Join(opt_ls, "\n\t"))
+
+		os.Exit(0)
+	}
+
+	if os.Args[1] == "--list" {
+		fmt.Println("Available:")
 		for _, soft := range tables {
-			fmt.Println("  - " + soft)
+			fmt.Println("* " + soft)
 		}
 		os.Exit(0)
 	}
+
+	if strings.HasPrefix(os.Args[1], "--") == true {
+		fmt.Println("Invalid option. See: --help")
+		os.Exit(0)
+	}
+
+	Program := os.Args[1]
+
+	if containsStr(tables, Program) != true {
+		fmt.Println("Sofware not in list.")
+		os.Exit(0)
+	}
+
+	rows, err := db.Query("SELECT * FROM " + Program)
+	check(err)
 
 	var data map[string][]map[string]string
 	data = make(map[string][]map[string]string)
@@ -95,7 +122,7 @@ func main() {
 		fmt.Println("+" + strings.Repeat("-", len(key)+2) + "+")
 		fmt.Println("| \033[1m" + strings.ToUpper(key) + "\033[0m |")
 		table := tablewriter.NewWriter(os.Stdout)
-		table.SetHeader([]string{"Comando", "Descricao"})
+		table.SetHeader([]string{"Command", "Description"})
 		table.SetRowLine(true)
 		table.SetRowSeparator("-")
 		for _, v := range value {
