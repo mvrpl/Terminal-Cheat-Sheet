c79ccb1a03b924f21c79c69ba05d59e5ef1bdd8c
Marcos Vinícius R. P. de Lima <mvrpl@MacBook-Pro-de-Marcos.local>
2017-02-16 23:21:06 -0200

    Modified RPM BUILD

--- RPMBUILD/run.sh
+++ RPMBUILD/run.sh
@@ -1,3 +1,16 @@
-yum install rpmdevtools -y
+yum install rpmdevtools wget -y
 echo '%_topdir %(pwd)' > ~/.rpmmacros
+wget https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz
+tar -xvf go1.8.linux-amd64.tar.gz -C /usr/local
+mkdir ~/.go
+export GOPATH=~/.go
+export GOROOT=/usr/local/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+go get github.com/mattn/go-sqlite3
+go get golang.org/x/text/runes
+cd /Terminal-Cheat-Sheet
+go build -o chsht
+tar cvfz chsht-0.1.4.tar.gz chsht
+mv chsht-0.1.4.tar.gz /Terminal-Cheat-Sheet/RPMBUILD/RPMS/x86_64
+cd /Terminal-Cheat-Sheet/RPMBUILD
 rpmbuild -ba SPECS/chsht.spec
