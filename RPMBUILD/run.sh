46e98f38bf25732da4bc94f339a492507c3a9e02
Marcos Vinícius Rodrigues Pires de Lima <mvro.dev@gmail.com>
2017-02-17 23:37:48 -0200

    Update run.sh

--- RPMBUILD/run.sh
+++ RPMBUILD/run.sh
@@ -1,7 +1,9 @@
+gover="1.8"
+chshtver="0.1.4"
 yum install rpmdevtools wget gcc -y
 echo '%_topdir %(pwd)' > ~/.rpmmacros
-wget https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz
-tar -xvf go1.8.linux-amd64.tar.gz -C /usr/local
+wget https://storage.googleapis.com/golang/go${gover}.linux-amd64.tar.gz
+tar -xvf go${gover}.linux-amd64.tar.gz -C /usr/local
 mkdir ~/.go
 export GOPATH=~/.go
 export GOROOT=/usr/local/go
@@ -10,7 +12,10 @@ go get github.com/mattn/go-sqlite3
 go get golang.org/x/text/runes
 cd /Terminal-Cheat-Sheet
 go build -o chsht
-tar cvfz chsht-0.1.4.tar.gz chsht
-mv chsht-0.1.4.tar.gz /Terminal-Cheat-Sheet/RPMBUILD/SOURCES
+mkdir chsht-${chshtver}
+mv chsht chsht-${chshtver}
+tar cvfz chsht-${chshtver}.tar.gz chsht-${chshtver}/
+mv chsht-${chshtver}.tar.gz /Terminal-Cheat-Sheet/RPMBUILD/SOURCES
 cd /Terminal-Cheat-Sheet/RPMBUILD
 rpmbuild -ba SPECS/chsht.spec
+rm -Rf go${gover}.linux-amd64.tar.gz
