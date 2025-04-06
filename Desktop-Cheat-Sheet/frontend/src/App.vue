e1b37a635df4a505e9851f2589c221b6494bb34a
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2025-04-06 20:15:38 -0300

    Add Desktop APP

---
+++ Desktop-Cheat-Sheet/frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import VerticalMenu from './components/Menu.vue';
+import ContentArea from './components/ContentArea.vue';
+</script>
+
+<template>
+  <div id="app" class="app-container">
+    <VerticalMenu />
+    <ContentArea />
+  </div>
+</template>
+
+<style>
+.app-container {
+  height: 100vh;
+}
+</style>
